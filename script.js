9eb811654cab90af3c13881d972869b967435a84
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-07-23 05:45:47 +0800

    Testing out with id's, still missing console printouts.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function btnClick(event) {
-  console.log("[btnClick] event.target.classList: " + event.target.classList);
+  // I find that using the classList would sometimes get a empty '' string
+  // on the console. This might cause problems later on.
+  console.log("[btnClick] event.target.id: " + event.target.id);
 }
 
 let btns = document.querySelectorAll(".button");
