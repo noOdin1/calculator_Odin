454844b9b06ead96a8183e635996c325ab03ce78
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-07-23 18:03:02 +0800

    Added numberArray and operatorArray for processing

--- script.js
+++ script.js
@@ -2,7 +2,37 @@ function btnClick(event) {
   // I find that using the classList would sometimes get a empty '' string
   // on the console. This might cause problems later on.
   console.log("[btnClick] event.target.id: " + event.target.id);
+  if (numberArray.includes(event.target.id)) {
+    console.log("[btnClick] This is a number");
+  }
+  if (operatorArray.includes(event.target.id)) {
+    console.log("[btnClick] This is an operator");
+  }
 }
 
+const numberArray = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "zero",
+  "point",
+  "zeroZero",
+];
+const operatorArray = [
+  "equals",
+  "plus",
+  "minus",
+  "multiplication",
+  "division",
+  "clear",
+  "percentage",
+  "squareRoot",
+];
 let docButtons = document.querySelectorAll("button");
 docButtons.forEach((btn) => btn.addEventListener("click", btnClick));
